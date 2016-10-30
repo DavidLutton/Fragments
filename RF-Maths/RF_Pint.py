4e6129afec93676d57453d5ec8594b3b3f718f60
David Lutton <david@dalun.space>
2016-10-30 22:10:42 +0000

    Add dBm > Watt, Watt > dBm

--- RF-Maths/RF_Pint.py
+++ RF-Maths/RF_Pint.py
@@ -30,12 +30,17 @@ with ureg.context('rf'):
     q = (230 * ureg.MHz)
     print(q.to('cm'))
 
-    # Some of the wanted combinations W V dBm `/ 50Ω`
     print()
+    # Some of the wanted combinations W V dBm `/ 50Ω`
     q = ((Z * (6.432 * ureg.watt)) ** .5)
     # ** .5) == square root
     print(q)
     print(q.to('V'))
+
+    print()
+    q = ((18 * ureg.volt) ** 2 / Z)
+    print(q)
+    print(q.to('W'))
 
     print()
     q = (13 * ureg.dBm) + (30 * ureg.dBm)
@@ -43,14 +48,27 @@ with ureg.context('rf'):
     print(q.to('dBW'))
 
     print()
-    '''q = (32 * ureg.dBW) + (30 * ureg.dBW)
+
+    q = 20 * ureg.dBm
     print(q)
-    print(q.to('dBm'))
-    '''
-    '''print()
-    q = (2 * ureg.dBW)
+    print(q.magnitude)
+    res = (10. ** ((q - 30)/10)) * ureg.watt
+    print(res)
+
+    print()
+    q = 10 * ureg.watt
     print(q)
-    '''
+    print(q.magnitude)
+    res = (10 * np.log10(q.magnitude / 0.001)) * ureg.dBm
+    print(res)
+    # q = (32 * ureg.dBW) + (30 * ureg.dBW)
+    # print(q)
+    # print(q.to('dBm'))
+
+    print()
+    # q = (2 * ureg.dBW)
+    # print(q)
+
 
 '''
     q = (18 * ureg.volt)
