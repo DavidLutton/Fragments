73d2fd20739cf7666f217972572fb06c699d2b9a
David Lutton <david@dalun.space>
2016-11-10 14:13:56 +0000

    Remove imports that are now in helpers

--- PodcastbyRequest/podcastbyrequest.py
+++ PodcastbyRequest/podcastbyrequest.py
@@ -4,57 +4,36 @@ import pickler
 from download import download
 
 import feedparser
-import requests
-
-import pprint
-import pickle
-import time
 import os
-
-
-def touch(fname, times=None):  # http://stackoverflow.com/a/1160227
-    with open(fname, 'a'):
-        os.utime(fname, times)
+import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
 pp = pp.pprint
 
 for URL in URLs:
-    name, url, storename, mode = URL
+    name, url, storename = URL
     print("####    " + name + "    ####")
 
-    # if os.path.isdir(storename) is not True:
-    #    mode = 't'
-
-    print(mode)
     delta = storename + ".pickle"
     feed = feedparser.parse(url)
 
     entries = pickler.feeddelta(feed, delta)
+    # pp(entries)
 
-    print(len(entries))
-
-    for each in entries:
-            print("\n")
-            for e in each:
-                pass  # print(e)
-
-            title = each.title
-
-            for links in each.links:
-                if links['type'].startswith("audio/"):
-                    try:
-                        download(title, links['href'], storename)
-                    except KeyError:
-                        print("Paradox by Paradox")
-            # print(each.guid)
-            # for media in each.media_content:
-            # print(each.title)
-            # print(media.url)
-
-            '''for media in each.media_content:
-                if media['filesize'] is not '0':
-                    print(each.title)
-                    print(media['url'])
-
-            '''
+    if os.path.isdir(storename) is not True:
+        print("  First run of " + name + " not downloading")
+        print("  To download back catalog delete " + delta + " and run again")
+        try:
+            os.makedirs(storename)
+        except OSError:
+            pass
+    else:
+        # print(len(entries))
+        for each in entries:
+                for links in each.links:
+                    if links['type'].startswith("audio/"):
+                        try:
+                            download(each.title, links['href'], storename)
+                        except KeyError:
+                            print("Paradox by Paradox")
+    print()
