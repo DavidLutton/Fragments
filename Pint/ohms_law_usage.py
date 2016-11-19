f3534bef36f0fb26f8f84de174906fe3de66b87b
David Lutton <david@dalun.space>
2016-11-19 14:43:09 +0000

    Add further examples to ohms_law usage

--- Pint/ohms_law_usage.py
+++ Pint/ohms_law_usage.py
@@ -9,3 +9,6 @@ ureg = UnitRegistry()
 ret = ohmslaw.RVI(48 * ureg.volt, 0.96 * ureg.amp)
 print(ret)
 print(str(ret.magnitude) + " " + str(ret.units))
+
+print(ohmslaw.VPR(50 * ureg.ohm, 6.432 * ureg.watt))  # 17.933209417167912 volt
+print(ohmslaw.PVR(18 * ureg.volt, 50 * ureg.ohm))  # 6.48 watt
