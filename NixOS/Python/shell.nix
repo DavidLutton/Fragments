5427e5e6da62467cc0a5f39312bc34abe8c5a274
David Lutton <David.Lutton@gmail.com>
2024-05-11 21:08:20 +0100

    Update Python shell.nix with issue links

--- NixOS/Python/shell.nix
+++ NixOS/Python/shell.nix
@@ -3,5 +3,7 @@ with import <nixpkgs> {};
 pkgs.mkShell {
   LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib libGL	];
 #  X11];
+# https://discourse.nixos.org/t/python-importerror-libstdc-so-6-cannot-open-shared-object-file-no-such-file-or-directory/36988
+# https://github.com/NixOS/nixpkgs/issues/250898
 
 }
